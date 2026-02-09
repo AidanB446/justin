ccf133b2bf29819911bc363fd463a2356168eb5f
Aidan Bruner <Aidanbruner789@gmail.com>
2026-02-09 16:57:55 -0500

    get_stock_price

---
+++ src/get_data.py
@@ -0,0 +1,35 @@
+
+from alpaca.common.exceptions import APIError
+
+from alpaca.data.requests import StockLatestTradeRequest
+from alpaca.data.historical import StockHistoricalDataClient
+    
+class Error :
+    def __init__(self, error_message) :
+        self.error_message = error_message
+
+def get_latest_price(user, stockSymbols) :
+    api_key = user.api_key  
+    api_secret= user.api_secret
+    
+    try :
+        client = StockHistoricalDataClient(api_key, api_secret)
+    except APIError as e :
+        raised_error = Error(e)        
+        return raised_error 
+
+    request_params = StockLatestTradeRequest(symbol_or_symbols=stockSymbols)
+
+    latest_trade_data = client.get_stock_latest_trade(request_params)
+    
+    return_dictionary = {}
+
+    for i in stockSymbols:
+        try :
+            return_dictionary[i] = latest_trade_data[i].price 
+        except KeyError :
+            return_dictionary[i] = None 
+
+    return return_dictionary
+
+
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 
 from user import User
+from get_data import get_latest_price
 
 if __name__ == "__main__":
-    api_key = "PKFASBVDSGPYDR4G7QPWCR47QDasdf"
-    api_secret = "BLqWTrzdvWdMDFmbRTenQM16goyZ1mJR8fK86qkEkRR2asdf"
+    api_key = "PKFASBVDSGPYDR4G7QPWCR47QD"
+    api_secret = "BLqWTrzdvWdMDFmbRTenQM16goyZ1mJR8fK86qkEkRR2"
 
     user = User(api_key=api_key, api_secret=api_secret, paper_trading=True) 
-
-    print(user)
-
+    
+    print(get_latest_price(user, ["AAPL", "TSLA", "MCSF"]))
 
 
