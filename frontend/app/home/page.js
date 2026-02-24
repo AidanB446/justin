8a87a5e273b856b214b6cf41982df52e8bd13f3a
Aidan Bruner <Aidanbruner789@gmail.com>
2026-02-23 19:59:33 -0500

    midworkl

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -4,17 +4,22 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 import User from "../components/user";
+import {doc} from "prettier";
 
 export default function Home() {
 	const [users, setUsers] = useState([]);
 	const [usernames, setUsernames] = useState([]);
+		
+	const [token, setToken] = useState("");
 
 	useEffect(() => {
 		const token = sessionStorage.getItem("token");
 
-		if (sessionStorage.getItem("token") === null) {
+		if (token === null) {
 			window.location.href = "/";
 		}
+		
+		setToken("token");
 
 		async function get_users() {
 			const url = "http://localhost:8000/get-all-users";
@@ -54,10 +59,42 @@ export default function Home() {
 
 		get_users();
 	}, []);
+	
+	function grabSelectedUsers() {
+		
+	}
+	
+	async function placeOrder() {
+		
+		const users = grabSelectedUsers(); // list of usernames
+		const stockSymbol = document.getElementById("stockSymbol");
+		const stockQty = document.getElementById("stockQty");
+		const stockChoice = document.getElementById("stockChoice");
+
+		const bodyData = {
+			users: users,
+			symbol: document.getElementById("stockSymbol").value,
+			qty: document.getElementById("stockQty").value,
+			side: document.getElementById("stockChoice").value,
+		};
+
+		const request = await fetch(
+			"http://localhost:8000/place_iterative_market_order",
+			{
+				method: "POST",
+				headers: {
+					Authorization: token,
+				},
+				body: JSON.stringify(bodyData),
+			},
+		);
+
+		const response = await request.json();
+		console.log(response);
+	}
 
 	return (
 		<div className={styles.page}>
-
 			<div className={styles.userDiv}>
 				<a href="/user-portal">User Portal</a>
 				<h2>Registered Accounts</h2>
@@ -75,19 +112,40 @@ export default function Home() {
 				<h1>Place Orders</h1>
 
 				<h3>Select Affected Users</h3>
-				<div className={styles.selectUsersDiv}>
+				<div id="userSelect" className={styles.selectUsersDiv}>
 					{usernames.map((name, index) => (
-						<div key={index} className="user-select-wrapper">
-							<label>
-								<input
-									type="checkbox"
-									id={"usernameboxselect_" + name}
-								/>
-								{name}
-							</label>
-						</div>
+						<label key={index}>
+							<input
+								type="checkbox"
+								id={"usernameboxselect_" + name}
+							/>
+							{name}
+							<br/>
+						</label>
 					))}
 				</div>
+				<div className={styles.mainParent}>
+					<h3>Place iterative Market Order</h3>
+					<input
+						type="text"
+						placeholder="Enter Stock Symbol"
+						id="stockSymbol"
+					/>
+					<br />
+					<input
+						type="text"
+						placeholder="Enter Stock Qty"
+						id="stockQty"
+					/>
+					<br />
+					<input
+						type="text"
+						placeholder="Enter Side Choice (buy or sell)"
+						id="stockChoice"
+					/>
+					<br />
+				</div>
+				<button>Place Limit Order</button>
 			</div>
 		</div>
 	);
